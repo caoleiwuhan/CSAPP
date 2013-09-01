f0910365ef60acb936029804577af3343775cfca
caoleiwuhan <caoleiwuhan@163.com>
2013-09-01 09:14:16 +0800

    add test function

---
+++ Lab1/tests.c
@@ -0,0 +1,117 @@
+/* Testing Code */
+
+#include <limits.h>
+#include <math.h>
+
+/* Routines used by floation point test code */
+
+/* Convert from bit level representation to floating point number */
+float u2f(unsigned u) {
+  union {
+    unsigned u;
+    float f;
+  } a;
+  a.u = u;
+  return a.f;
+}
+
+/* Convert from floating point number to bit-level representation */
+unsigned f2u(float f) {
+  union {
+    unsigned u;
+    float f;
+  } a;
+  a.f = f;
+  return a.u;
+}
+
+int test_bitAnd(int x, int y)
+{
+  return x&y;
+}
+int test_bitOr(int x, int y)
+{
+  return x|y;
+}
+int test_isTmax(int x) {
+    return x == 0x7FFFFFFF;
+}
+int test_isZero(int x) {
+  return x == 0;
+}
+int test_fitsBits(int x, int n)
+{
+  int TMin_n = -(1 << (n-1));
+  int TMax_n = (1 << (n-1)) - 1;
+  return x >= TMin_n && x <= TMax_n;
+}
+int test_getByte(int x, int n)
+{
+    unsigned char byte;
+    switch(n) {
+    case 0:
+      byte = x;
+      break;
+    case 1:
+      byte = x >> 8;
+      break;
+    case 2:
+      byte = x >> 16;
+      break;
+    default:
+      byte = x >> 24;
+      break;
+    }
+    return (int) (unsigned) byte;
+}
+int test_isNegative(int x) {
+  return x < 0;
+}
+int test_addOK(int x, int y)
+{
+    long long lsum = (long long) x + y;
+    return lsum == (int) lsum;
+}
+int test_isGreater(int x, int y)
+{
+  return x > y;
+}
+int test_replaceByte(int x, int n, int c)
+{
+    switch(n) {
+    case 0:
+      x = (x & 0xFFFFFF00) | c;
+      break;
+    case 1:
+      x = (x & 0xFFFF00FF) | (c << 8);
+      break;
+    case 2:
+      x = (x & 0xFF00FFFF) | (c << 16);
+      break;
+    default:
+      x = (x & 0x00FFFFFF) | (c << 24);
+      break;
+    }
+    return x;
+}
+int test_rotateLeft(int x, int n) {
+  unsigned u = (unsigned) x;
+  int i;
+  for (i = 0; i < n; i++) {
+      unsigned msb = u >> 31;
+      unsigned rest = u << 1;
+      u = rest | msb;
+  }
+  return (int) u;
+}
+int test_bitCount(int x) {
+  int result = 0;
+  int i;
+  for (i = 0; i < 32; i++)
+    result += (x >> i) & 0x1;
+  return result;
+}
+int test_isNonZero(int x)
+{
+  return x!=0;
+}
